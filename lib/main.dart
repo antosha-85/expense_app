f63ffa17d84affd36ea60910cd6d260f28208aca
Anton Smirnov <antosha-85@yandex.ru>
2021-05-12 00:38:38 -0600

    Applied some styling for my card

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,37 @@ class MyApp extends StatelessWidget {
             Column(
               children: transactions.map((transaction) {
                 return Card(
-                  child: Text(transaction.title),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          transaction.amount.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            transaction.title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                          ),
+                          Text(transaction.date.toString(), style: TextStyle(color: Colors.grey),)
+                        ],
+                      )
+                    ],
+                  ),
                 );
               }).toList(),
             ),
