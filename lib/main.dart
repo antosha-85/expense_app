7a20918d529411192210c53532f56f3014c28b2e
Anton Smirnov <antosha-85@yandex.ru>
2021-05-12 00:05:05 -0600

    Created a new transaction class and output titles of newly initialized transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'transaction.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceries',
+        amount: 16.99,
+        date: DateTime.now()),
+  ];
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -23,9 +33,13 @@ class MyApp extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              child: Text('LIST OF TRANSACTIONS'),
-            )
+            Column(
+              children: transactions.map((transaction) {
+                return Card(
+                  child: Text(transaction.title),
+                );
+              }).toList(),
+            ),
           ],
         ),
       ),
